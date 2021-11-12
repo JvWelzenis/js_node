88d2ad65681b7947bd89a8cb1a41e8a9e2565773
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-11-12 16:23:23 +0100

    Add files via upload

---
+++ js_node/script.js
@@ -0,0 +1,14 @@
+console.log('hello winc academy students')
+
+// this is a large sum
+
+/*
+let multi= 1230941 * 1823794;
+console.log(multi);
+
+let division = 637263 / 54;
+console.log(division);
+*/
+// node ignors comments
+
+// no ofcourse not, node ignors comments
